2dbfeee20b43c2434f4eac25a38372858b815aab
r4s0n3 <github@miomideal.com>
2026-08-07 23:57:43 +0200

    commission added

--- prisma/migrations/20260727010000_reuse_gallery_media/migration.sql
+++
@@ -1,2 +0,0 @@
--- Allow one uploaded media object to be reused by multiple gallery posts.
-DROP INDEX IF EXISTS "GalleryPost_imageUrl_key";
--- prisma/migrations/20260728000000_gallery_post_images/migration.sql
+++
@@ -1,6 +0,0 @@
-ALTER TABLE "GalleryPost"
-ADD COLUMN "images" JSONB NOT NULL DEFAULT '[]';
-
-ALTER TABLE "GalleryPost"
-ADD CONSTRAINT "GalleryPost_images_array"
-CHECK (jsonb_typeof("images") = 'array');
